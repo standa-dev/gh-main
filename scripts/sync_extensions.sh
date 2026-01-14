eee6915b0e7856d000bba123c301ea6a1ebec6c3
Standa Musil <standa@nimbus.co>
2026-01-14 08:37:44 -0800

    implement raw sync CI

---
+++ scripts/sync_extensions.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+AUTH_URL="https://x-access-token:${GH_TOKEN}@github.com/${SYNC_REPOSITORY}.git"
+
+# Trailing slashes mean contents of that folder
+rsync -a "$SYNC_SRC_DIR/" "./"
+
+# Commit & push if needed
+if [[ -n "$(git status --porcelain)" ]]; then
+  echo "Changes detected. Creating commit..."
+
+  git config user.email "nimbus-deploy@nimbus.co"
+  git config user.name "Nimbus Deploy"
+
+  git add --all
+  git commit -m "Update extension"
+
+  echo "Pushing to extension repository..."
+  git push "$AUTH_URL" HEAD
+  echo "✅ Sync complete."
+else
+  echo "No changes to sync. ✅"
+fi
